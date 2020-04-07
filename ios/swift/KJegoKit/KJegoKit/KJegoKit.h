9dc79c3c6e788b318b5595a48a4b725b057ad9e2
yulu.kong <yulu.kong@ubtrobot.com>
2020-04-07 15:32:04 +0800

    init project

---
+++ ios/swift/KJegoKit/KJegoKit/KJegoKit.h
@@ -0,0 +1,19 @@
+//
+//  KJegoKit.h
+//  KJegoKit
+//
+//  Created by yulu kong on 2020/4/7.
+//  Copyright © 2020 yulu kong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KJegoKit.
+FOUNDATION_EXPORT double KJegoKitVersionNumber;
+
+//! Project version string for KJegoKit.
+FOUNDATION_EXPORT const unsigned char KJegoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KJegoKit/PublicHeader.h>
+
+
